ade747c4680769115367bbc4284a6c82dbed985a
Andreas Viborg <andreas.viborg@maxar.com>
2023-02-16 12:01:38 +0100

    wip

--- README.md
+++ README.md
@@ -2,9 +2,31 @@
 
 https://code.visualstudio.com/docs/cpp/config-mingw
 
+https://stackoverflow.com/questions/70197831/unable-to-determine-what-cmake-generator-to-use-vs-code-windows-10
+
 
 https://github.com/Microsoft/vcpkg/
-vcpkg install json-c
-vcpkg integrate install
+.\vcpkg.EXE install json-c protobuf-c[tools] eigen3 --triplet=x64-windows
+
+If it fails (see here: https://github.com/microsoft/vcpkg/issues/29677)
+do this patch in your vcpkg directory
+```git
+diff --git a/ports/protobuf-c/portfile.cmake b/ports/protobuf-c/portfile.cmake
+index 0a972cb27..4bcce7017 100644
+--- a/ports/protobuf-c/portfile.cmake
++++ b/ports/protobuf-c/portfile.cmake
+@@ -26,7 +26,7 @@ vcpkg_copy_pdbs()
+
+ if("tools" IN_LIST FEATURES)
+     vcpkg_copy_tools(
+-        TOOL_NAMES protoc-gen-c protoc-c
++        TOOL_NAMES protoc-gen-c
+         AUTO_CLEAN
+     )
+ endif()
+```
+ .\vcpkg.exe integrate install
 
 https://www.40tude.fr/how-to-use-vcpkg-with-vscode-and-cmake/
+
+add C:\vcpkg\installed\x64-windows\tools\protobuf-c to PATH
--- src/parrot-client.cpp
+++ src/parrot-client.cpp
@@ -1,8 +1,42 @@
 #include <ulog.h>
-#include <libpomp.h>
+#include <pdraw-vsink/pdraw_vsink.h>
 
-int main(){
-    ULOGI("Hello %s\n", pomp_event_str(POMP_EVENT_CONNECTED));
+
+int main(int argc, char **argv){
+    int status = EXIT_SUCCESS, res, i;
+	struct pdraw_vsink *vsink = NULL;
+	struct pdraw_media_info *media_info = NULL;
+
+	if (argc < 2) {
+		ULOGE("usage: %s <url>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	res = pdraw_vsink_start(argv[1], &media_info, &vsink);
+	if (res < 0 || media_info == NULL) {
+		ULOG_ERRNO("pdraw_vsink_start", -res);
+		exit(EXIT_FAILURE);
+	}
+	ULOGI("started");
+
+	ULOGI("media_info: name=%s, path=%s",
+	      media_info->name,
+	      media_info->path);
+	ULOGI("media_info: duration=%.3fs, res=%ux%u, framerate=%u/%u",
+	      media_info->duration / 1000000.0,
+	      media_info->video.raw.info.resolution.width,
+	      media_info->video.raw.info.resolution.height,
+	      media_info->video.raw.info.framerate.num,
+	      media_info->video.raw.info.framerate.den);
+
+
+    res = pdraw_vsink_stop(vsink);
+	if (res < 0)
+		ULOG_ERRNO("pdraw_vsink_stop", -res);
+	/* media_info won't be valid after pdraw_vsink_stop() */
+
+	ULOGI("%s", (status == EXIT_SUCCESS) ? "success!" : "failed!");
+	exit(status);
 
     return 0;
 }
